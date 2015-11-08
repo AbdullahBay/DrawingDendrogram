a01b9f10e927a54fe4718e8bec414717401e99f4
Abdullah masaüstü <abdulahsalih>
2015-11-08 17:14:59 +0200

    Çizimler panel ortamına aktarıldı. Analiz roporundaki ilk 5 madde için kullanılacak araçlar seçildi.

--- DrawingDendrogram/TestDrawing/Form1.Designer.cs
+++ DrawingDendrogram/TestDrawing/Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(262, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 270);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(283, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "     ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 402);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "sdjksadklsajdks sadkdjskldjskd jkdj";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(569, 466);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- DrawingDendrogram/TestDrawing/Form1.cs
+++ DrawingDendrogram/TestDrawing/Form1.cs
@@ -15,13 +15,7 @@ namespace TestDrawing
         public Form1()
         {
             InitializeComponent();
-            calismaAlaniYukseklik = this.Height;
-            /*Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
-            System.Drawing.Graphics formGraphics;
-            formGraphics = this.CreateGraphics();
-            formGraphics.DrawLine(pen, 20, 10, 300, 100);
-            pen.Dispose();
-            formGraphics.Dispose();*/
+            calismaAlaniYukseklik = this.panel1.Height;
         }
         int MyMargin = 150;
         int Yukseklik = 100;
@@ -34,48 +28,18 @@ namespace TestDrawing
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+           
+        }
+        private Point PointFromRef(Point RefPoint, int x,int y)
+        {
+            Point returnPoint = new Point(RefPoint.X + x, RefPoint.Y + y);
+            return returnPoint;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
             Brush myBrush = new SolidBrush(System.Drawing.Color.Blue);
             Pen pen = new Pen(myBrush, 6);
-            /*Point RefPoint;
-            //1
-            RefPoint= new Point(MyMargin, YukseklikHesapla(Yukseklik));
-            
-            Point solUst = RefPoint;
-            Point solAlt = PointFromRef(solUst, 0, Yukseklik);
-            Point sagUst = PointFromRef(solUst, Genislik, 0);
-            Point sagAlt = PointFromRef(solUst, Genislik, Yukseklik);
-            Point FramePoint1 = Point.Add(RefPoint, new Size(Genislik/2,0));
-            
-            e.Graphics.DrawLine(pen, solUst, solAlt);
-            e.Graphics.DrawLine(pen, sagUst, sagAlt);
-            e.Graphics.DrawLine(pen, sagUst, solUst);
-            //2
-            Yukseklik = 75;
-            RefPoint = PointFromRef(sagUst, Bosluk, 25);
-
-            solUst = RefPoint;
-            solAlt = PointFromRef(solUst, 0, Yukseklik);
-            sagUst = PointFromRef(solUst, Genislik, 0);
-            sagAlt = PointFromRef(solUst, Genislik, Yukseklik);
-            Point FramePoint2 = Point.Add(RefPoint, new Size(Genislik / 2, 0));
-
-            e.Graphics.DrawLine(pen, solUst, solAlt);
-            e.Graphics.DrawLine(pen, sagUst, sagAlt);
-            e.Graphics.DrawLine(pen, sagUst, solUst);
-
-            Yukseklik = 150;
-
-            solUst = new Point((Size)FramePoint1);
-            solUst.Y = YukseklikHesapla(Yukseklik);
-            sagUst = new Point((Size)FramePoint2);
-            sagUst.Y = YukseklikHesapla(Yukseklik);
-            solAlt = FramePoint1;
-            sagAlt = FramePoint2;
-
-            e.Graphics.DrawLine(pen, solUst, solAlt);
-            e.Graphics.DrawLine(pen, sagUst, sagAlt);
-            e.Graphics.DrawLine(pen, sagUst, solUst);
-            */
             // ders 9 
             //1. frame
             Size ortaNokta;
@@ -83,7 +47,7 @@ namespace TestDrawing
             Point solAlt = new Point(50, YukseklikHesapla(0));
             Point sagAlt = new Point(70, YukseklikHesapla(0));
 
-            Point solUst ;
+            Point solUst;
             Point sagUst;
             solUst = new Point((Size)solAlt);
             solUst.Y = YukseklikHesapla(Yukseklik);
@@ -139,12 +103,14 @@ namespace TestDrawing
             e.Graphics.DrawLine(pen, solUst, solAlt);
             e.Graphics.DrawLine(pen, sagUst, sagAlt);
             e.Graphics.DrawLine(pen, sagUst, solUst);
+            // soldan aşşağıya doğru metin yazıyor
+            string Name ="sdasas";
+            var g = e.Graphics;
+            g.DrawString(Name, new Font("Tahoma", 8), Brushes.Black, 0, 0,
+            new StringFormat(StringFormatFlags.DirectionVertical));
+          
 
         }
-        private Point PointFromRef(Point RefPoint, int x,int y)
-        {
-            Point returnPoint = new Point(RefPoint.X + x, RefPoint.Y + y);
-            return returnPoint;
-        }
+
     }
 }
